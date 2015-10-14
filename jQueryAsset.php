bc1089bcdd39b3fd1aeac4f477f499422f025675
Alecsnder Scherbina <aleon880@gmail.com>
2015-10-14 22:59:20 +0300

    first commit

---
+++ jQueryAsset.php
@@ -0,0 +1,13 @@
+<?php
+namespace plirex\assets;
+
+class JQueryAsset extends CoreAsset
+{
+    public $sourcePath = '@common/assets/components/jQuery';
+    public $baseUrl = '@realPath';
+    public $js = [
+        'js/jquery.js',
+    ];
+    public $css = [
+    ];
+}
